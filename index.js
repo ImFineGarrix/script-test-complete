3d47c60e8b67b62bb6116d42632bdf445457cfdd
Chanon Rugdee <Chanon_Rug@freewillgroup.com>
2025-07-02 16:05:25 +0700

    :sparkles: change order and controller

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const directLocation = {
 	enter: '[Enter]',
 	left: '[Left]',
 	right: '[Right]',
-	prtSc: '[PrtSc]',
-	f2: '[F2]',
-	numSlash: '[NumSlash]',
+	prtSc: '[PrtSc]', // View Order
+	f2: '[F2]',  
+	numSlash: '[NumSlash]', // Cancel Order
+	home: '[Home]' // Change Price
 	s: '^s',
 	b: '^b',
 	buyin: '[Buyin]',
@@ -13,8 +14,8 @@ const directLocation = {
 	'2firm': '[PTD-2FIRM]'
 }
 
-const onDelayDirector = (direct) => {
-	Delay(1000)
+const onDelayDirector = (direct, delay = 1000) => {
+	Delay(delay)
 	onScreenObj.Keys(directLocation[direct])
 }
 
@@ -112,7 +113,7 @@ const sendNormalOrder = (side, stock, price, volume, account, publish, condition
 	}
 }
 
-const send2FirmAndBuyinOrder = (action, stock, price, volume, account1, account2, brokerId, controlKey) => {
+const send1FirmAndBuyinOrder = (action, stock, price, volume, account1, account2, brokerId, controlKey) => {
 	onDelayDirector(action)
 	onScreenObj.Keys(stock)
 	onDelayDirector('enter')
@@ -134,37 +135,102 @@ const send2FirmAndBuyinOrder = (action, stock, price, volume, account1, account2
 		onScreenObj.Keys(nvdr2)
 	}
 	onDelayDirector('enter')
-	onScreenObj.Keys(controlKey || '')
+  if (controlKey) {
+    onScreenObj.Keys(controlKey)
+  }
 	onDelayDirector('enter')
 	onDelayDirector('enter')
 }
 
-const send1FirmOrder = (side, stock, price, volume, account, brokerId, controlKey) => {
+const send2FirmOrder = (side, stock, price, volume, account, nvdr, brokerId, controlKey) => {
 	onDelayDirector('1firm')
 	onScreenObj.Keys(side)
 	onDelayDirector('enter')
 	onScreenObj.Keys(stock)
 	onDelayDirector('enter')
+	onScreenObj.Keys(volume)
+  onDelayDirector('enter')
 	onScreenObj.Keys(price)
-
+  onDelayDirector('enter')
+  if (nvdr) {
+    onScreenObj.Keys(nvdr)
+  }
+  onDelayDirector('enter')
+  onScreenObj.Keys(brokerId || '00U8')
+  onDelayDirector('enter')
+  onScreenObj.Keys(account)
+  onDelayDirector('enter')
+  if (controlKey) {
+    onScreenObj.Keys(controlKey)
+  }
+	onDelayDirector('enter')
+	onDelayDirector('enter')
+	onDelayDirector('enter')
 }
 
 const changeOrder = (ordno, price, volume, publish) => {
+  onDelayDirector('home')
+  if (price) {
+    onScreenObj.Keys(price)
+  }
+  onDelayDirector('enter')
+  if (volume) {
+    onScreenObj.Keys(volume)
+  }
+  onDelayDirector('enter')
+  if (publish) {
+    onScreenObj.Keys(publish)
+  }
+  onDelayDirector('enter')
+  onDelayDirector('enter')
+  Log.Message(`Change Order OrdNo ${ordno}: ${price ? `Price: ${price}`: ''} ${volume ? `Price: ${volume}`: ''} ${publish ? `Price: ${publish}`: ''}`)
+}
 
+const changeAccount = (ordno, account, nvdr) => {
+  if (account) {
+    onScreenObj.Keys(account)
+  }
+  onDelayDirector('enter')
+  if (nvdr) {
+    onScreenObj.Keys(nvdr)
+  }
+  onDelayDirector('enter')
+  onDelayDirector('enter')
+  Log.Message(`Change Account Ordno ${ordno}: ${account ? `Account: ${account}` : ''} ${nvdr ? `NVDR: ${nvdr}` : ''}`)
 }
 
 const cancelOrder = (ordno) => {
-	onDelayDirector('prtSc')
-	onDelayDirector('left')
-	onScreenObj.Keys(ordno)
-	onDelayDirector('enter')
 	onDelayDirector('numSlash')
 	onScreenObj.Keys('y')
 	onDelayDirector('enter')
 	Log.Message(`Cancel Order => ordno: ${ordno}`)
 }
 
-const main = () => {}
+const mainOrder = (no, action, side, stock, price, volume, account, publish, condition, date, nvdr, ot, account1, account2, brokerId, controlKey) => {
+  if (action.toLowerCase() === 'new') {
+    sendNormalOrder(side, stock, price, volume, account, publish, condition, date, nvdr , ot)
+  }
+  if (action.toLowerCase() === '1firm' || action.toLowerCase() === 'buyin') {
+    send1FirmAndBuyinOrder(action, stock, price, volume, account1, account2, brokerId, controlKey)
+  }
+  if (action.toLowerCase() === '2firm') {
+    send2FirmOrder(side, stock, price, volume, account, nvdr, brokerId, controlKey)
+  }
+  if (action.toLowerCase() === 'change') {
+    onDelayDirector('prtSc')
+    onDelayDirector('left')
+    onDelayDirector(ordno)
+    onDelayDirector('enter')
+    if (price || volume || publish) {
+      changeOrder(ordno, price, volume, publish)
+    }
+    if (account || nvdr) {
+      changeAccount(ordno, account, nvdr)
+    }
+  }
+  if (action.toLowerCase() === 'cancel')
+}
 
+const mainExpect = () => {}
 
 
